532dff76b8daf9188061d663f51a98a925d7774c
DESKTOP-OSFKUPN\hi <rhksgns4@gmail.com>
2024-11-04 11:36:53 +0900

    <11.04 SERVLET>

--- chap11-filter-lecture-source/src/main/java/com/ohgiraffers/filter/FirstFilterTestServlet.java
+++ chap11-filter-lecture-source/src/main/java/com/ohgiraffers/filter/FirstFilterTestServlet.java
@@ -5,13 +5,26 @@ import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(value = "/first/filter")
 public class FirstFilterTestServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        
+        System.out.println("doGet 메소드 호출 확인됨...");
+
+        PrintWriter out = resp.getWriter();
+        out.println("<!doctype html>");
+        out.println("<html>");
+        out.println("<head></head>");
+        out.println("<body>");
+        out.println("<h3>필터 확인 용 서블릿 요청 확인</h3>");
+        out.println("</body>");
+        out.println("</html>");
+
+        out.flush();
+        out.close();
 
     }
 }
