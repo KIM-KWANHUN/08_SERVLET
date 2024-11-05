626e1cbd5ecbb7920139215de3d1652f06f2651e
DESKTOP-OSFKUPN\hi <rhksgns4@gmail.com>
2024-11-05 09:53:43 +0900

    <24.11.05 SEVLET >

---
+++ TEST1/src/main/java/com/ohgiraffers/test1/testServlet.java
@@ -0,0 +1,29 @@
+package com.ohgiraffers.test1;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.Date;
+
+@WebServlet("/test")
+public class testServlet extends HttpServlet {
+    public testServlet() {
+        System.out.println("기본생성자 호출됨...");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String name = req.getParameter("name");
+        int age = Integer.parseInt(req.getParameter("age"));
+        String date = req.getParameter("birth");
+        String gender = req.getParameter("gender");
+        
+
+
+    }
+}
---
+++ TEST1/src/main/webapp/index.jsp
@@ -0,0 +1,21 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+<form action="test" method="post">
+    <label>이름 : </label><input type="text" name="name">
+    <br>
+    <label>나이 : </label><input type="number" name="age">
+    <br>
+    <label>생일 : </label><input type="date" name="birth">
+    <br>
+    <label>성별 : </label>
+    <input type="radio" name="gender" id="male" value="M">
+    <label for="male">남성</label>
+    <input type="radio" name="gender" id="female" value="F">
+    <label for="female">여성</label>
+</body>
+</html>
